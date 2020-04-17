4aa318288f258d485e9e2d8a552ac41aca11853f
Victor <victorgilasp@gmail.com>
2020-04-17 03:31:38 +0200

    Complejidad password
    Se establece dentro del método de registro , los requisitos que debe cumplir la contraseña.
    Se ha intentado buscar la mínima complejidad usando un condicional if -else.

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -12,10 +12,18 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
-    }
+	    if(password.matches(".*[0-9]{1,}.*") && password.matches(".*[~!@#$%^&*()_-].*") && password.matches(".*[A-Z]{1,}.*") && password.matches(".*[a-z]{1,}.*") && password.length()>=8)
+                    {
+                        User u = new User(username, HashUtils.getMd5(password), type, -1);
+       				    u.save();
+        			    registerComplete();
+                    }
+                    else
+                    {
+                       	flash.put("error", Messages.get("Public.register.validation.password.complexity"));
+				        register();
+                    }
+       }
 
     public static void registerComplete(){
         render();
