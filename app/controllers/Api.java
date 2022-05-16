e6db64b31b4858c3868d9f3a425902cb9ce5c343
leku2020 <43129366+leku2020@users.noreply.github.com>
2022-05-16 23:51:59 +0200

    fix: Api
    Broken Function-Level Authorization - API

--- app/controllers/Api.java
+++ app/controllers/Api.java
@@ -1,14 +1,31 @@
 package controllers;
 
-
 import com.google.gson.JsonObject;
 import models.User;
 import play.mvc.Controller;
+import models.Constants;
+import models.User;
 
 public class Api extends Controller {
 
-    public static void removeAllUsers(){
-        User.removeAll();
-        renderJSON(new JsonObject());
+    public void removeAllUsers(){
+        if(accesoPermitido()){
+            User.removeAll();
+        } else {
+            render("errors/403.html");
+        }
+    }
+    public boolean accesoPermitido (){
+        if (session.contains("username")){
+            User a = User.loadUser(session.get("username"));
+            if (a != null){
+                renderArgs.put("user", a);
+                User u = (User) renderArgs.get("user");
+                if (u.getType().equals(Constants.User.TEACHER)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
