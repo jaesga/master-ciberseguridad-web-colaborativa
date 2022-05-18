6cde099a628e0b83eb5a0e0f9613c93ade03ab3a
Ángel Heredia <33127189+anthares101@users.noreply.github.com>
2022-05-18 16:57:51 -0400

    Avoid register process from overwriting users

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -12,12 +12,21 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
+        User safeUser = User.loadUser(username);
+        if (safeUser != null){
+            registerError();
+            return;
+        }
+        User newUser = new User(username, HashUtils.getMd5(password), type, -1);
+        newUser.save();
         registerComplete();
     }
 
     public static void registerComplete(){
+        render();
+    }
+
+    public static void registerError(){
         render();
     }
 }
