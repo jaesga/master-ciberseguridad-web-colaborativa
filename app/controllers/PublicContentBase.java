f661227b13b734a4246101f1668a7e32402510a3
javi-ag <153739397+javi-ag@users.noreply.github.com>
2023-12-13 16:49:30 +0100

    Update PublicContentBase.java

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -11,9 +11,15 @@ public class PublicContentBase extends Controller {
         render();
     }
 
-    public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
+    public static void processRegister(String username, String password, String passwordCheck, String type) {
+
+            // Genera un salt aleatorio
+            String salt = HashUtils.generateRandomSalt();
+            // Crea el usuario con el hash de la contraseña y el salt
+            String hashedPassword = HashUtils.getPBKDF2Hash(password, salt);
+            User u = new User(username, hashedPassword, salt, type, -1);
         u.save();
+   
         registerComplete();
     }
 
