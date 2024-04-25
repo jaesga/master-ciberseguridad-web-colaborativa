7afa462c1c167006fef0e4a5b79657cc23c9b858
Jose Luis Juarez <jljuarezmanzano@gmail.com>
2024-04-25 18:12:51 +0200

    Correccion de la Vulnerabilidad - escalamiento de privilegios
    Signed-off-by: jljuarezmanzano <jljuarezmanzano@gmail.com>

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -13,8 +13,15 @@ public class PublicContentBase extends Controller {
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
         User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        boolean userExist = u.save();
+        if(!userExist){
+            registerFailed();
+        }else {
+            registerComplete();
+        }
+    }
+    public static void registerFailed(){
+        render();
     }
 
     public static void registerComplete(){
