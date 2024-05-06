434ea52b73bf838859f15b1f89b6a5c45dde1f43
Javi Espinosa <javier.espinosagar@gmail.com>
2024-05-06 19:11:47 +0200

    Fix VULN Tutoria 3

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -7,16 +7,27 @@ import play.mvc.Controller;
 
 public class PublicContentBase extends Controller {
 
+    public static final int MIN_PASSWORD_LENGTH = 10;
+
     public static void register(){
         render();
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        if (isPasswordValid(password)){
+            User u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+            registerComplete();
+        } else {
+            flash.error("Password should be at least 10 chars.");
+            register();
+        }
     }
 
+    private static boolean isPasswordValid(String password){
+        return password.length() >= MIN_PASSWORD_LENGTH;
+    }
+    
     public static void registerComplete(){
         render();
     }
