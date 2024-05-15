d3d6d8f8fa15c4aea9ec68270ef507ba6a815b3b
Antonioag123 <antonioalonsogarciia@gmail.com>
2024-05-15 19:02:24 -0400

    Validación para tipo de usuario en formulario de registro

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -12,9 +12,18 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+       // En el caso de que no este vacio y no sea nulo el tipo de usuario se guardara
+        if(type != null && !type.isEmpty()){
+
+            User u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+            registerComplete();
+        } else{
+             // En el caso contrario mostrara un error
+            flash.error("Debe seleccionar el tipo de usuario.");
+            register();
+
+        }
     }
 
     public static void registerComplete(){
